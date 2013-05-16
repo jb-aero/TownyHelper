8c1194105753ef0e238b493c408f2c15475580ba
Jim Bilbrey <jb_aero@verizon.net>
2013-05-16 03:51:57 -0400

    Added towny_towns, towny_canbuild, and towny_canbreak

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-TownyHelper
-===========
+# TownyHelper
 
 A Towny-hooking extension for CommandHelper
+
+## Functions
+
+### towny_towns
+```
+Returns: array
+Arguments: 
+Description: Returns an array where the keys are town names and the values are
+ arrays with keys mayor(string), assistants(array of strings), residents(array of strings),
+ nation(string or null), spawn(locationArray or null)
+```
+
+### towny_canbuild
+```
+Returns: boolean
+Arguments: player, location
+Description: Checks if a player has build permissions at a location.
+```
+
+### towny_canbreak
+```
+Returns: boolean
+Arguments: player, location
+Description: Checks if a player has destroy permissions at a location.
+```
--- src/main/java/com/zeoldcraft/CHTowny.java
+++ src/main/java/com/zeoldcraft/CHTowny.java
@@ -1,12 +1,17 @@
 package com.zeoldcraft;
 
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
 import com.laytonsmith.PureUtilities.Version;
 import com.laytonsmith.abstraction.MCPlugin;
 import com.laytonsmith.abstraction.bukkit.BukkitMCLocation;
+import com.laytonsmith.annotations.api;
 import com.laytonsmith.core.CHVersion;
 import com.laytonsmith.core.ObjectGenerator;
 import com.laytonsmith.core.Static;
 import com.laytonsmith.core.constructs.CArray;
+import com.laytonsmith.core.constructs.CBoolean;
 import com.laytonsmith.core.constructs.CNull;
 import com.laytonsmith.core.constructs.CString;
 import com.laytonsmith.core.constructs.Construct;
@@ -16,10 +21,14 @@ import com.laytonsmith.core.exceptions.ConfigRuntimeException;
 import com.laytonsmith.core.functions.AbstractFunction;
 import com.laytonsmith.core.functions.Exceptions.ExceptionType;
 import com.palmergames.bukkit.towny.Towny;
+import com.palmergames.bukkit.towny.db.TownyDataSource;
 import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
 import com.palmergames.bukkit.towny.exceptions.TownyException;
 import com.palmergames.bukkit.towny.object.Resident;
 import com.palmergames.bukkit.towny.object.Town;
+import com.palmergames.bukkit.towny.object.TownyUniverse;
+import com.palmergames.bukkit.towny.object.TownyPermission.ActionType;
+import com.palmergames.bukkit.towny.utils.PlayerCacheUtil;
 
 public class CHTowny {
 
@@ -37,7 +46,24 @@ public class CHTowny {
 		}
 	}
 	
+	public static TownyDataSource getTownyData(Target t) {
+		Static.checkPlugin("Towny", t);
+		return TownyUniverse.getDataSource();
+	}
+	
+	public static Resident getResident(String name, Target t) {
+		try {
+			return getTownyData(t).getResident(name);
+		} catch (NotRegisteredException nre) {
+			throw new ConfigRuntimeException("Unknown resident " + name, ExceptionType.PluginInternalException, t);
+		}
+	}
+	
 	public abstract static class TownyFunction extends AbstractFunction {
+
+		public ExceptionType[] thrown() {
+			return new ExceptionType[]{ExceptionType.InvalidPluginException};
+		}
 
 		public boolean isRestricted() {
 			return true;
@@ -50,19 +76,20 @@ public class CHTowny {
 		public Version since() {
 			return CHVersion.V3_3_1;
 		}
+
+		public String getName() {
+			String cname = this.getClass().getName();
+			return cname.substring(cname.indexOf("$") + 1, cname.length());
+		}
 	}
 	
-	public static class get_towns extends TownyFunction {
+	@api
+	public static class towny_towns extends TownyFunction {
 
-		public ExceptionType[] thrown() {
-			return new ExceptionType[]{ExceptionType.FormatException};
-		}
-
-		public Construct exec(Target t, Environment environment,
-				Construct... args) throws ConfigRuntimeException {
-			Towny towny = getTowny(t);
+		public Construct exec(Target t, Environment environment, Construct... args) throws ConfigRuntimeException {
+			TownyDataSource data = getTownyData(t);
 			CArray ret = new CArray(t);
-			for (Town town : towny.getTownyUniverse().getTowns()) {
+			for (Town town : data.getTowns()) {
 				CArray atown = new CArray(t);
 				atown.set("mayor", new CString(town.getMayor().getName(), t), t);
 				CArray assis = new CArray(t);
@@ -94,10 +121,6 @@ public class CHTowny {
 			return ret;
 		}
 
-		public String getName() {
-			return "get_towns";
-		}
-
 		public Integer[] numArgs() {
 			return new Integer[]{0};
 		}
@@ -105,6 +128,46 @@ public class CHTowny {
 		public String docs() {
 			return "array {} Returns an associative array with the names of all towns as keys."
 					+ " At each key is an array of information about the town.";
+		}
+	}
+	
+	public static boolean construct(Construct cplayer, Construct cloc, ActionType action, Target t) {
+		Location loc = (Location) ObjectGenerator.GetGenerator().location(cloc, null, t).getHandle();
+		Player player = (Player) Static.GetPlayer(cplayer, t).getHandle();
+		if (TownyUniverse.getTownBlock(loc) == null)
+			return true;
+		return PlayerCacheUtil.getCachePermission(player, loc, loc.getBlock().getTypeId(), action);
+	}
+	
+	@api
+	public static class towny_canbuild extends TownyFunction {
+
+		public Construct exec(Target t, Environment environment, Construct... args) throws ConfigRuntimeException {
+			return new CBoolean(construct(args[0], args[1], ActionType.BUILD, t), t);
+		}
+
+		public Integer[] numArgs() {
+			return new Integer[]{2};
+		}
+
+		public String docs() {
+			return "boolean {player, location} Checks if a player has build permissions at a location";
+		}
+	}
+	
+	@api
+	public static class towny_canbreak extends TownyFunction {
+
+		public Construct exec(Target t, Environment environment, Construct... args) throws ConfigRuntimeException {
+			return new CBoolean(construct(args[0], args[1], ActionType.DESTROY, t), t);
+		}
+
+		public Integer[] numArgs() {
+			return new Integer[]{2};
+		}
+
+		public String docs() {
+			return "boolean {player, location} Checks if a player has destroy permissions at a location";
 		}
 	}
 }
